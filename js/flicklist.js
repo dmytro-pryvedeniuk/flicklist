bebee7f1064182342495dc45f69d2bf62b9c4f7c
Dmytro Pryvedeniuk <dmytro.pryvedeniuk@gmail.com>
2018-04-30 19:38:34 +0300

    Load images from external source

--- js/flicklist.js
+++ js/flicklist.js
@@ -1,27 +1,41 @@
+(function() {
 
+    var api = {
+        root: "https://api.themoviedb.org/3",
+        token: "047e4a4b47e76df08b73db22d719eba6",
+        uriBase: "https://image.tmdb.org/t/p/w500"
+    }
 
-var api = {
-	root: "https://api.themoviedb.org/3",
-	token: "" // TODO put your api key here
-}
+    function testTheAPI() {
+        $.ajax({
+            url: api.root + "/discover/movie",
+            data: {
+                api_key: api.token
+            },
+            success: buildList
+        });
+    }
 
-/**
- * Makes an AJAX request to themoviedb.org, asking for some movies
- * if successful, prints the results to the console
- */
-function testTheAPI() {
-	$.ajax({
-		url: api.root + "/discover/movie",
-		data: {
-			api_key: api.token,
-		},
-		success: function(response) {
-			console.log("We got a response from The Movie DB!");
-			console.log(response);
-		}
-	});
-}
+    function getImage()
+    {
 
+    }
+    
+    function buildList(response) {
+        console.log("There is response from The Movie DB!")
+        console.log(response);
+        const container = $("#container");
+        
+        for (let i in response.results)
+        {
+            let video = response.results[i];
+            container.append(`
+                <div class="poster">
+                    <img src="${api.uriBase + video.poster_path}"</img>
+                    <p>${video.title} (${video.vote_average})</p>
+                </div>`)
+        }
+    }
 
-console.log("The script loaded!");
-testTheAPI();
+    testTheAPI();
+}());
